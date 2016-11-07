6e5191bbe481177566915001f656dd695ab46df1
Klucinka007 <Palkovicova.lucia@gmail.com>
2016-11-07 21:28:56 +0100

    Validation in register component

--- src/app/register.component.ts
+++ src/app/register.component.ts
@@ -9,22 +9,61 @@ import {Component} from "@angular/core/src/metadata/directives";
 <div class="container">
 <div class="form-group">
   <label for="email">Email</label>
-  <input type="text" class="form-control" id="email" required
+  <input type="text" class="form-control" id="email" required minlength="6" maxlength="24" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$"
   [(ngModel)]="email" name="email" #name="ngModel">
-  <div [hidden]="name.valid || name.pristine" class="alert alert-danger">Name required</div>
+
+  <div *ngIf="name.errors && (name.dirty || name.touched)"  class="alert alert-danger">
+  
+  <div [hidden]="!name.errors.required">
+     Name is required
+  </div>
+  <div [hidden]="!name.errors.minlength">
+     Name must be at least 6 characters long.
+  </div>
+  <div [hidden]="!name.errors.maxlength">
+     Name cannot be more than 24 characters long.
+  </div>
+  <div [hidden]="!name.errors.pattern">
+     Please enter emaial in right format(yyyy@yyyy.yy)
+  </div>
 </div>
       <div class="form-group">
   <label for="password">Password</label>
-  <input type="text" class="form-control" id="password">
-
+  <input type="password" class="form-control" id="password" required minlength="6" maxlength="24" pattern="(?=.*[a-z])(?=.*[A-Z]).{8,}"
+  [(ngModel)]="password" a="password" #a="ngModel">
+  <div *ngIf="a.errors && (a.dirty || a.touched)"  class="alert alert-danger">
+  <div [hidden]="!a.errors.required">
+     Name is required
+  </div>
+  <div [hidden]="!a.errors.minlength">
+     Name must be at least 6 characters long.
+  </div>
+  <div [hidden]="!a.errors.maxlength">
+     Name cannot be more than 24 characters long.
+  </div>
+    <div [hidden]="!a.errors.pattern">
+    Include one capital and one number character
+  </div>
+  </div>
       </div>
       
       <div class="form-group">
-  <label for="name">Name</label>
-  <input type="text" class="form-control" id="name">
+  <label for="fullname">Name</label>
+  <input type="text" class="form-control" id="fullname" required minlength="6" maxlength="24"
+  [(ngModel)]="fullname" b="fullname" #b="ngModel">
+   <div *ngIf="b.errors && (b.dirty || b.touched)"  class="alert alert-danger"> 
+   <div [hidden]="!b.errors.minlength">
+     Name must be at least 6 characters long.
+  </div>
+  <div [hidden]="!b.errors.maxlength">
+     Name cannot be more than 24 characters long.
+  </div>
+   </div>
   <button type="button" class="btn btn-success">Submit</button>
   </div>
 </div>
+  </div>
+
 `,
   styles:[`
   .btn{
