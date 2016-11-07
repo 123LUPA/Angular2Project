9ace27ef0141183dae61efdc26938a00821b3969
Klucinka007 <Palkovicova.lucia@gmail.com>
2016-11-07 18:30:36 +0100

    Merged with login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,53 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {RouterModule} from "@angular/router";
+import {LoginComponent} from "./login.component";
+import {RegisterComponent} from "./register.component";
 import {DashboardComponent} from "./dashboard.component";
 import {BooksService} from "./books.service";
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegisterComponent,
     DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+
     RouterModule.forRoot([
+      {
+        path:'login',
+        component: LoginComponent
+      },
 
       {
-        path: 'dashboard',
-        component: DashboardComponent
+        path:'register',
+        component: RegisterComponent
       },
       {
+
+        path: 'dashboard',
+
+        component: DashboardComponent
+
+      },
+
+      {
+
         path: '',
+
         redirectTo: '/dashboard',
+
         pathMatch: 'full'
+
       },
     ])
 
-        ],
+  ],
   providers: [BooksService],
   bootstrap: [AppComponent]
 })
--- src/app/register.component.ts
+++ src/app/register.component.ts
@@ -1,3 +1,38 @@
 /**
- * Created by Palko on 07/11/2016.
+ * Created by Palko on 06/11/2016.
  */
+
+import {Component} from "@angular/core/src/metadata/directives";
+@Component({
+  selector:'my-register',
+  template:`
+<div class="container">
+<div class="form-group">
+  <label for="email">Email</label>
+  <input type="text" class="form-control" id="email" required
+  [(ngModel)]="email" name="email" #name="ngModel">
+  <div [hidden]="name.valid || name.pristine" class="alert alert-danger">Name required</div>
+</div>
+      <div class="form-group">
+  <label for="password">Password</label>
+  <input type="text" class="form-control" id="password">
+
+      </div>
+      
+      <div class="form-group">
+  <label for="name">Name</label>
+  <input type="text" class="form-control" id="name">
+  <button type="button" class="btn btn-success">Submit</button>
+  </div>
+</div>
+`,
+  styles:[`
+  .btn{
+   margin-top: 10px;
+   }`]
+
+})
+
+export class RegisterComponent{
+
+}
